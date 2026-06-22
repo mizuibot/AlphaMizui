15157f56a2efbbfc3961dc305340ed7b5d8675c8
mizuibot <injustamenteb447@gmail.com>
2026-06-22 17:13:25 -0300

    Corrige sistema XP

--- commands/prefixmizui/xpsystem.js
+++ commands/prefixmizui/xpsystem.js
@@ -1,11 +1,18 @@
 const fs = require("fs");
 
-const xpFile = "./xp.json";
+const XP_FILE = "./xp.json";
 
 function loadXP() {
+  if (!fs.existsSync(XP_FILE)) {
+    fs.writeFileSync(
+      XP_FILE,
+      JSON.stringify({}, null, 2)
+    );
+  }
+
   return JSON.parse(
     fs.readFileSync(
-      xpFile,
+      XP_FILE,
       "utf8"
     )
   );
@@ -13,7 +20,7 @@ function loadXP() {
 
 function saveXP(data) {
   fs.writeFileSync(
-    xpFile,
+    XP_FILE,
     JSON.stringify(
       data,
       null,
@@ -22,9 +29,7 @@ function saveXP(data) {
   );
 }
 
-function createUser(
-  userId
-) {
+function createUser(userId) {
 
   const data = loadXP();
 
@@ -48,6 +53,8 @@ function createUser(
 
       class: "none",
 
+      multiplier: 1,
+
       lastMessage: 0,
 
       lastDaily: 0
@@ -59,11 +66,99 @@ function createUser(
   return data;
 }
 
+function getMultiplier(level) {
+
+  if (level >= 75) return 4;
+
+  if (level >= 50) return 3;
+
+  if (level >= 25) return 2;
+
+  return 1;
+}
+
+function xpNeeded(level) {
+
+  return Math.floor(
+    100 * Math.pow(level, 1.5)
+  );
+}
+
+function addXP(userId, amount) {
+
+  const data =
+    createUser(userId);
+
+  const user =
+    data[userId];
+
+  user.multiplier =
+    getMultiplier(
+      user.level
+    );
+
+  const gainedXP =
+    Math.floor(
+      amount *
+      user.multiplier
+    );
+
+  user.xp += gainedXP;
+
+  user.messages++;
+
+  let leveledUp = false;
+
+  while (
+    user.xp >=
+    xpNeeded(user.level)
+  ) {
+
+    user.xp -=
+      xpNeeded(user.level);
+
+    user.level++;
+
+    leveledUp = true;
+  }
+
+  saveXP(data);
+
+  return {
+
+    level:
+      user.level,
+
+    xp:
+      user.xp,
+
+    gainedXP,
+
+    leveledUp
+  };
+}
+
+function getUser(userId) {
+
+  const data =
+    createUser(userId);
+
+  return data[userId];
+}
+
 module.exports = {
 
   loadXP,
 
   saveXP,
 
-  createUser
+  createUser,
+
+  addXP,
+
+  getUser,
+
+  xpNeeded,
+
+  getMultiplier
 };
